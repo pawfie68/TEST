681e32aa2db68c258462952d1d60d760d768d103
pawfie68 <pawfie68@gmail.com>
2017-04-05 20:22:17 +0200

    Add files via upload
    1st commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using System.Data.Entity;
+
+
+
+
+class Napis
+
+{
+    public int ID { get; set; }
+    public string napis { get; set; }
+}
+
+class DbConection : DbContext
+{
+    public DbConection() : base(@"Data Source=(local)\MYINSTANCE;Initial Catalog=MyTestDB;Integrated Security=True") { }
+    public DbSet<Napis> Napis { get; set; } 
+}
+
+class MainClass
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            var txt = new Napis
+            {
+                napis = Convert.ToString(Console.ReadLine())
+            };
+            var dbConection = new DbConection();
+            dbConection.Napis.Add(txt);
+            dbConection.SaveChanges();
+ 
+        }
+    }
+}
